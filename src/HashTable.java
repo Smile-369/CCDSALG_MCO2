91a095a5bee9cbfca841321ee0847df6285baf0c
nicoleUY <106289210+nicoleUY@users.noreply.github.com>
2022-12-09 01:27:21 +0800

    file output works na

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,7 +1,9 @@
 //Design an algorithm which, given an input DNA string of length n and size k of substrings, 
 //uses a hash table of size n to compute its k-mer distribution.
 
-//source: https://www.geeksforgeeks.org/implementing-our-own-hash-table-with-separate-chaining-in-java/
+//sources: https://www.geeksforgeeks.org/implementing-our-own-hash-table-with-separate-chaining-in-java/
+//http://www.java2s.com/Code/Java/Development-Class/FNVHash.htm
+//https://rosettacode.org/wiki/CRC-32#Java
 
 //Java program to demonstrate implementation of our
 //own hash table with chaining for collision detection
@@ -12,9 +14,8 @@ import java.util.zip.CRC32;
 
 //Class to represent entire hash table
 class Map<K, V> {
-
 	// bucketArray is used to store array of chains
-	private ArrayList<HashNode<K, V> > bucketArray;
+	private ArrayList<HashNode<K, Integer> > bucketArray;
 
 	// Current capacity of array list
 	private int numBuckets;
@@ -38,175 +39,156 @@ class Map<K, V> {
 	public int size() { return size; }
 	public boolean isEmpty() { return size() == 0; }
 	
-	
-	//=========================================================HERE=============
-	
-	
-	//THIS IS THE PART WHERE THE OTHER HASH CODE COMES IN
-	//probs put it in as another method
-	private final int hashCode (K key) {
-		return Objects.hashCode(key);
-	}
+	//HASH CODES
+	private final int hashCode (K key, int choice) {
+		long startTime=System.nanoTime();
 
-	//FNV1A HASH FUNCTION
-	private final int hashFNV(String key) {
-		final int FNV_32_INIT = 0x811c9dc5;
-	    final int FNV_32_PRIME = 0x01000193;
-	    
-	    byte[] keyInBytes = key.getBytes(StandardCharsets.UTF_8);
-	    
-	    int rv = FNV_32_INIT;
-        final int len = keyInBytes.length;
-        for(int i = 0; i < len; i++) {
-            rv ^= keyInBytes[i];
-            rv *= FNV_32_PRIME;
-        }
-		return rv;
+		switch(choice) {
+		
+		case 1: //built-in hashCode
+			
+			long endTime=System.nanoTime();
+			long elapsedTime=endTime-startTime;
+			System.out.println(elapsedTime+"ns");
+			
+			return Objects.hashCode(key);
+			
+		case 2: //FNV1a32
+			final int FNV_32_INIT = 0x811c9dc5;
+		    final int FNV_32_PRIME = 0x01000193;
+		    
+		    byte[] keyInBytes = ((String) key).getBytes(StandardCharsets.UTF_8);
+		    
+		    int rv = FNV_32_INIT;
+	        final int len = keyInBytes.length;
+	        for(int i = 0; i < len; i++) {
+	            rv ^= keyInBytes[i];
+	            rv *= FNV_32_PRIME;
+	        }
+	        
+	        endTime=System.nanoTime();
+			elapsedTime=endTime-startTime;
+			System.out.println(elapsedTime+"ns");
+			
+			return rv;
+			
+		case 3: //CRC32
+			int crc;
+		    CRC32 myCRC = new CRC32( ) ;
+		    myCRC.update( ((String) key).getBytes( ) ) ;
+		  	crc = (int) myCRC.getValue();
+		  	
+		  	endTime=System.nanoTime();
+			elapsedTime=endTime-startTime;
+			System.out.println(elapsedTime+"ns");
+			
+		  	return crc;
+		default: 
+			endTime=System.nanoTime();
+			elapsedTime=endTime-startTime;
+			System.out.println(elapsedTime+"ns");
+			
+			return 0;
+		}
+		
 	}
-	
-	//CRC32 HASH FUNCTION
-	private final int hashCRC32(String key) {
-		int crc;
-	    CRC32 myCRC = new CRC32( ) ;
-	    myCRC.update( key.getBytes( ) ) ;
-	    //System.out.println( "The CRC-32 value is : " + Long.toHexString( myCRC.getValue( ) ) + " !" ) ;
-	  	crc = (int) myCRC.getValue();
-	  	return crc;
-	}
-	
-	
-	//============================================================================
-	
-	
 	
 	// This implements hash function to find index
 	// for a key
-	private int getBucketIndex(K key)
+	private int getBucketIndex(K key, int choice)
 	{
-		int hashCode = hashCode(key);
+		long startTime=System.nanoTime();
+		
+		int hashCode = hashCode(key, choice);
 		int index = hashCode % numBuckets;
 		// key.hashCode() could be negative.
 		index = index < 0 ? index * -1 : index;
+		
+		long endTime=System.nanoTime();
+		long elapsedTime=endTime-startTime;
+		System.out.println(elapsedTime+"ns");
+		
 		return index;
 	}
 
-	// Method to remove a given key
-	public V remove(K key)
-	{
-		// Apply hash function to find index for given key
-		int bucketIndex = getBucketIndex(key);
-		int hashCode = hashCode(key);
-		// Get head of chain
-		HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-		// Search for key in its chain
-		HashNode<K, V> prev = null;
-		while (head != null) {
-			// If Key found
-			if (head.key.equals(key) && hashCode == head.hashCode)
-				break;
-
-			// Else keep moving in chain
-			prev = head;
-			head = head.next;
-		}
-
-		// If key was not there
-		if (head == null)
-			return null;
-
-		// Reduce size
-		size--;
-
-		// Remove key
-		if (prev != null)
-			prev.next = head.next;
-		else
-			bucketArray.set(bucketIndex, head.next);
-
-		return head.value;
-	}
-
 	// Returns value for a key
-	public V get(K key)
+	public int get(K key, int choice)
 	{
 		long startTime=System.nanoTime();
 		// Find head of chain for given key
-		int bucketIndex = getBucketIndex(key);
-		int hashCode = hashCode(key);
+		int bucketIndex = getBucketIndex(key, choice);
+		int hashCode = hashCode(key, choice);
 	
-		HashNode<K, V> head = bucketArray.get(bucketIndex);
+		HashNode<K, Integer> head = bucketArray.get(bucketIndex);
 
 		// Search key in chain
 		while (head != null) {
 			if (head.key.equals(key) && head.hashCode == hashCode) {
 				long endTime=System.nanoTime();
 				long elapsedTime=endTime-startTime;
-				System.out.println(elapsedTime);
+				System.out.println(elapsedTime+"ns");
 				return head.value;
 			}
+				
 			head = head.next;
 		}
 
 		// If key not found
-		return null;
+		return -1;
 	}
 
 	// Adds a key value pair to hash
-	public void add(K key, V value)
+	public void add(K key, int value, int choice)
 	{
+		long startTime=System.nanoTime();
+
 		// Find head of chain for given key
-		int bucketIndex = getBucketIndex(key);
-		int hashCode = hashCode(key);
-		HashNode<K, V> head = bucketArray.get(bucketIndex);
+		int bucketIndex = getBucketIndex(key, choice);
+		int hashCode = hashCode(key, choice);
+		HashNode<K, Integer> head = bucketArray.get(bucketIndex);
 
 		// Check if key is already present
 		while (head != null) {
 			if (head.key.equals(key) && head.hashCode == hashCode) {
-				head.value = value; //change to head.value += value;?=============================here=====
+				head.value += value; //change to head.value += value;?=============================here=====
+				
+				long endTime=System.nanoTime();
+				long elapsedTime=endTime-startTime;
+				System.out.println(elapsedTime+"ns");
+				
 				return;
 			}
 			head = head.next;
 		}
 
+		startTime=System.nanoTime();
+		
 		// Insert key in chain
 		size++;
 		head = bucketArray.get(bucketIndex);
-		HashNode<K, V> newNode
-			= new HashNode<K, V>(key, value, hashCode);
+		HashNode<K, Integer> newNode
+			= new HashNode<K, Integer>(key, value, hashCode);
 		newNode.next = head;
 		bucketArray.set(bucketIndex, newNode);
-
+		
+		long endTime=System.nanoTime();
+		long elapsedTime=endTime-startTime;
+		System.out.println(elapsedTime+"ns");
+		
 			}
-	// Driver method to test Map class
 	public void create(){
 		Map<String, Integer> map = new Map<>(0);
 	}
-	public static void create(String[] input,Map map){
+	public void create(String[] input, Map<String, Integer> map, int choice){
+		long startTime=System.nanoTime();
+		
 		for(int i = 0 ; i < input.length;i++){
-			map.add(input[i],i);
+			map.add(input[i],1,choice);
 		}
+		
+		long endTime=System.nanoTime();
+		long elapsedTime=endTime-startTime;
+		System.out.println(elapsedTime+"ns");
 	}
-	public static void main(String[] args)
-	{
-		//================================================================HERE
-		//search later on which java lib Map<>() is in
-		Substring sns =new Substring();
-		String thing=sns.createRandomString((int)Math.pow(10,4));
-		Map<String, Integer> map = new Map<>(thing.length());
-//		map.add("this", 1);
-//		map.add("coder", 2);
-//		map.add("this", 4);
-//		map.add("hi", 5);
-//		;
-		String[] input = sns.kmerArrayCreation(thing,6);
-		Map.create(input,map);
-		System.out.println(map.size());
-		System.out.println(map.remove("this"));
-		System.out.println(map.remove(input[2]));
-		System.out.println(map.remove("this"));
-		System.out.println(map.size());
-		System.out.println(map.isEmpty());
-		System.out.println(map.get(input[23]));
-	}
+
 }
